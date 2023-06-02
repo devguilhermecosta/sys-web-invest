9ea416226eb5ef2975aa15741c7306121d93b629
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-06-01 21:49:10 -0300

    style: modified the logic for styled input in focus

--- base_static/global/js/script.js
+++ base_static/global/js/script.js
@@ -2,16 +2,17 @@
   const formInput = document.querySelectorAll('.C-login_input');
   
   formInput.forEach((input) => {
+    const inputLength = input.value.length;
     const labelSister = input.nextElementSibling;
     const className = 'C-login_input_active';
 
-    if (input.value.length >= 1) {
+    if (inputLength >= 1) {
       labelSister.classList.add(className);
     }
 
     input.addEventListener('change', function() {
-      const inputLength = input.value.length;
-      if (inputLength >= 1) {
+      const inputLengthInside = input.value.length;
+      if (inputLengthInside >= 1) {
         labelSister.classList.add(className);
       } else {
         labelSister.classList.remove(className);
