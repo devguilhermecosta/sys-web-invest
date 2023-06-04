39fa3cffba434f5c2ed5ea721b9008123d95bfa5
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-06-04 02:32:53 -0300

    update: created the custom views reset password

--- resetpassword/urls.py
+++ resetpassword/urls.py
@@ -1,22 +1,18 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
+from . import views
 
 
 urlpatterns: list = [
     path('password_reset/',
-         auth_views.PasswordResetView.as_view(
-              template_name="resetpassword/pages/password_reset.html"),
+         views.PasswordReset.as_view(),
          name="password_reset"),
     path('password_reset/done/',
-         auth_views.PasswordResetDoneView.as_view(
-              template_name="resetpassword/pages/password_reset_done.html"),
+         views.PasswordResetDone.as_view(),
          name="password_reset_done"),
     path('reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(
-              template_name="resetpassword/pages/password_reset_confirm.html"),
+         views.PasswordResetConfirm.as_view(),
          name='password_reset_confirm'),
     path('reset/done/',
-         auth_views.PasswordResetCompleteView.as_view(
-              template_name="resetpassword/pages/password_reset_complet.html"),
+         views.PasswordResetComplete.as_view(),
          name='password_reset_complete'),
 ]
--- resetpassword/views.py
+++ resetpassword/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.contrib.auth import views
+from django.contrib import messages
+from .forms.reset_password import (
+    PasswordResetCustomForm,
+    SetPasswordCustomForm,
+    )
 
-# Create your views here.
+
+class PasswordReset(views.PasswordResetView):
+    template_name = 'resetpassword/pages/password_reset.html'
+    form_class = PasswordResetCustomForm
+
+
+class PasswordResetDone(views.PasswordResetDoneView):
+    template_name = 'resetpassword/pages/password_reset_done.html'
+
+    def get(self, *args, **kwargs):
+        super_get = super().get(*args, **kwargs)
+        messages.success(
+            self.request,
+            (
+                'Você receberá as instruções necessárias para a redefinição '
+                'de sua senha. Se você não recer o e-mail de recuperação de '
+                'senha, por favor, verifique se digitou o e-mail correto e '
+                'verifique seu spam.')
+        )
+        return super_get
+
+
+class PasswordResetConfirm(views.PasswordResetConfirmView):
+    template_name = 'resetpassword/pages/password_reset_confirm.html'
+    form_class = SetPasswordCustomForm
+
+
+class PasswordResetComplete(views.PasswordResetCompleteView):
+    template_name = 'resetpassword/pages/password_reset_complet.html'
+
+    def get(self, *args, **kwargs):
+        super_get = super().get(*args, **kwargs)
+        messages.success(
+            self.request,
+            'Sua senha foi alterada com sucesso.'
+        )
+        return super_get
