c76c5d44b5329a36866cdd02e47161546b21d5e1
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-06-06 00:20:37 -0300

    feat: created a function for make chrome broser - Selenium

---
+++ utils/browser/selenium.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.webdriver import WebDriver
+from pathlib import Path
+import sys
+import os
+
+
+ROOT_PATH = Path(__file__).parent.parent.parent
+if sys.platform == 'linux':
+    CHROMEDRIVER_NAME = 'chromedriver_linux'
+else:
+    CHROMEDRIVER_NAME = 'chromedriver.exe'
+CHROMEDRIVER_PATH = str(ROOT_PATH / 'bin' / CHROMEDRIVER_NAME)
+
+
+def make_chrome_browser(*options) -> WebDriver:
+    chrome_options: ChromeOptions = webdriver.ChromeOptions()
+    chrome_service: Service = Service(
+        executable_path=CHROMEDRIVER_PATH,
+    )
+
+    for option in options:
+        chrome_options.add_argument(option)
+
+    if os.environ.get('SELENIUM_HEADLESS') == '1':
+        chrome_options.add_argument('--headless')
+
+    browser: Chrome = webdriver.Chrome(
+        service=chrome_service,
+        options=chrome_options,
+        )
+
+    return browser
