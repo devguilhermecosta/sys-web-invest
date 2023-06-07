1559b71c280aa37ced4e90b13c42375fdfe8a94a
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-06-06 21:57:41 -0300

    test: created aditional tests

---
+++ resetpassword/tests/test_password_reset_complete_tests.py
@@ -0,0 +1,235 @@
+from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from . import (
+    password_reset_confirm_url_name,
+    password_reset_url_name
+)
+
+
+'''
+Testing the password reset was not easy.
+I decided to keep the code repeating
+in tests to keep them more readable.
+'''
+
+
+class PasswordResetCompletTests(TestCase):
+    def setUp(self) -> None:
+        # Simulate a client
+        self.c = Client()
+
+        # Creating a new user
+        self.user_data: dict = {
+            'first_name': 'Jhon',
+            'last_name': 'Dhoe',
+            'username': 'jhondoe',
+            'email': 'jhon@email.com',
+            'password': 'Jhon123@',
+        }
+        self.user: User = User.objects.create(**self.user_data)
+        self.user.save()
+        return super().setUp()
+
+    def test_password_reset_returns_campo_obrigatório_if_not_email(self) -> None:  # noqa: E501
+        '''When the user tries to recover the password
+        without informing the email, an exception
+        is raised'''
+        response = self.c.post(
+            reverse(password_reset_url_name),
+            {
+                'email': '',
+            }
+        )
+        content = response.content.decode('utf-8')
+        self.assertIn('Campo obrigatório', content)
+
+    def test_password_reset_returns_a_message_error_if_user_not_found(self) -> None:  # noqa: E501
+        '''If user not found by email, a message error is raised'''
+        response = self.client.post(
+            reverse(password_reset_url_name),
+            {
+                'email': 'jhondoe@email.com',
+            }
+        )
+        content = response.content.decode('utf-8')
+        self.assertIn(
+            'Nenhum usuário cadastrado com este e-mail',
+            content,
+        )
+
+    def test_password_reset_status_code_302_if_email_ok(self) -> None:
+        '''This test checks of the url was redirected.
+        Check destination url and the status code.'''
+        response = self.c.post(
+            reverse(password_reset_url_name),
+            {
+                'email': self.user.email,
+            },
+            follow=True,
+        )
+        self.assertEqual(
+            response.redirect_chain[0][1], 302
+        )
+        self.assertEqual(
+            response.redirect_chain[0][0],
+            '/password/password_reset/done/'
+        )
+
+    def test_password_reset_returns_a_success_message_if_email_swas_sent(self) -> None:  # noqa: E501
+        response = self.c.post(
+            reverse(password_reset_url_name),
+            {
+                'email': self.user.email,
+            },
+            follow=True,
+        )
+        content = response.content.decode('utf-8')
+        self.assertIn(
+            (
+                'Você receberá as instruções necessárias para '
+                'a redefinição de sua senha. Se você não recer '
+                'o e-mail de recuperação de senha, por favor, '
+                'verifique se digitou o e-mail correto e verifique '
+                'seu spam.'
+                ),
+            content,
+        )
+
+    def test_password_reset_password_recovery_url_is_correct(self) -> None:
+        '''This test check the url that was sent in
+        the email. The url hides the token'''
+        response = self.c.post(
+            reverse(password_reset_url_name),
+            {
+                'email': self.user.email,
+            },
+        )
+        uid = response.context['uid']
+        token = response.context['token']
+
+        response_url_get = self.client.get(
+            reverse(
+                password_reset_confirm_url_name,
+                kwargs={
+                    'uidb64': uid,
+                    'token': token,
+                }),
+        )
+        self.assertEqual(
+            response_url_get.url,
+            '/password/reset/MQ/set-password/',
+        )
+
+    def test_password_reset_returns_error_messages_if_new_password_is_incorrect(self) -> None:  # noqa: E501
+        '''
+        You need to use the url generated in the get request with
+        argument (path) for the post request because of the token.
+
+        Must return error message if:
+        - password length is less then 8 chars,
+        - password doesn't have at least an uppercase letter,
+        a lowercase letter, and a number'''
+        response = self.c.post(
+            reverse(password_reset_url_name),
+            {
+                'email': self.user.email,
+            },
+        )
+        uid = response.context['uid']
+        token = response.context['token']
+
+        response_url_post = self.client.get(
+            reverse(
+                password_reset_confirm_url_name,
+                kwargs={
+                    'uidb64': uid,
+                    'token': token,
+                }),
+        )
+        response_url_post = self.client.post(
+                response_url_post.url,
+                {
+                    'new_password1': 'abc',
+                    'new_password2': 'abdd',
+                }
+                ),
+
+        content = response_url_post[0].content.decode('utf-8')
+        self.assertIn('A senha precisa ter pelo menos 8 catacteres',
+                      content
+                      )
+        self.assertIn('Pelo menos uma letra maíscula', content)
+        self.assertIn('Pelo menos uma letra minúscula', content)
+        self.assertIn('Pelo menos um número.', content)
+
+    def test_password_reset_returns_error_message_if_passwords_are_different(self) -> None:  # noqa: E501
+        '''
+        You need to use the url generated in the get request with
+        argument (path) for the post request because of the token.
+        Returns "The passwords must be equal." '''
+        response = self.c.post(
+            reverse(password_reset_url_name),
+            {
+                'email': self.user.email,
+            },
+        )
+        uid = response.context['uid']
+        token = response.context['token']
+
+        response_url_post = self.client.get(
+            reverse(
+                password_reset_confirm_url_name,
+                kwargs={
+                    'uidb64': uid,
+                    'token': token,
+                }),
+        )
+
+        response_url_post = self.client.post(
+                response_url_post.url,
+                {
+                    'new_password1': 'Abcd123456',
+                    'new_password2': 'Abcd12345678',
+                }
+                )
+
+        content = response_url_post.content.decode('utf-8')
+        self.assertIn('As senhas precisam ser iguais.', content)
+
+    def test_password_reset_returns_message_success_if_password_is_changed_succesfully(self) -> None:  # noqa: E501
+        '''
+        You need to use the url generated in the get request with
+        argument (path) for the post request because of the token.
+        It's necessary to uses follow=True for message success test.
+        '''
+        response = self.c.post(
+            reverse(password_reset_url_name),
+            {
+                'email': self.user.email,
+            },
+        )
+        uid = response.context['uid']
+        token = response.context['token']
+
+        response_url_post = self.client.get(
+            reverse(
+                password_reset_confirm_url_name,
+                kwargs={
+                    'uidb64': uid,
+                    'token': token,
+                }),
+        )
+
+        response_url_post = self.client.post(
+                response_url_post.url,
+                {
+                    'new_password1': 'Abcd123456',
+                    'new_password2': 'Abcd123456',
+                },
+                follow=True,
+                )
+
+        content = response_url_post.content.decode('utf-8')
+        self.assertIn('Sua senha foi alterada com sucesso.', content)
--- resetpassword/tests/test_password_reset_functional_tests.py
+++ resetpassword/tests/test_password_reset_functional_tests.py
@@ -1,4 +1,5 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.test import Client
 from django.urls import reverse
 from django.contrib.auth.models import User
 from django.core import mail
@@ -12,13 +13,15 @@ import pytest
 
 
 @pytest.mark.functional_test
-class PasswordResetFuncionalTests(StaticLiveServerTestCase):
-    """
-        This test entire set of views required for
-        password recovery.
-        This is a long test.
-    """
+class ResetPasswordFunctionalTest(StaticLiveServerTestCase):
     def setUp(self) -> None:
+        # Simulate a client
+        self.c = Client()
+
+        # Making a chrome browser with Selenium
+        self.browser = make_chrome_browser()
+
+        # Creating a new User
         self.user_data: dict = {
             'first_name': 'Jhon',
             'last_name': 'Dhoe',
@@ -30,27 +33,20 @@ class PasswordResetFuncionalTests(StaticLiveServerTestCase):
         self.user.save()
         return super().setUp()
 
-    def test_password_reset_change_user_password_succesfully(self) -> None:
-        # 1) Calling the reset password view with get
-        # This is not necessaty, but, it's good for test
-        response_get = self.client.get(
-            reverse(password_reset_url_name)
-        )
-        self.assertEqual(response_get.status_code, 200)
+    def tearDown(self) -> None:
+        # Quiting a chrome browser
+        self.browser.quit()
+        return super().tearDown()
 
-        # 2) Then we post the response with the email
-        # If not used with 'email', the status code
-        # we will 200, but, it's necessary 302 redirect
-        # Then, i check if status code is 302
-        # Then i check if the email was sent
+    def test_password_reset_change_user_password_succesfully(self) -> None:
+        # 1) Sending password recovery email
         response_post = self.client.post(
             reverse(password_reset_url_name), {
                 'email': self.user_data['email'],
             },
             )
-        self.assertEqual(response_post.status_code, 302)
 
-        self.assertEqual(len(mail.outbox), 1)
+        # 2) Checking if the password recovery email was sent
         self.assertIn(
             'Redefinição de senha em testserver',
             mail.outbox[0].subject,
@@ -60,68 +56,41 @@ class PasswordResetFuncionalTests(StaticLiveServerTestCase):
         uid: str = response_post.context['uid']
         token: str = response_post.context['token']
 
-        # 4) Now we make a get reverse using the uid and token
-        # So i simulate the user by clicking on password reset
-        # link sent in the email
-        response_url_reset_get = self.client.get(
-            reverse(password_reset_confirm_url_name,
-                    kwargs={
-                        'uidb64': uid,
-                        'token': token,
-                    }),
-            follow=True,
-        )
-        self.assertEqual(
-            response_url_reset_get.status_code,
-            200,
-            )
-
-        response_url_reset_get = self.client.post(
-            reverse(password_reset_confirm_url_name,
-                    kwargs={
-                        'uidb64': uid,
-                        'token': token,
-                    }),
-            {
-                'new_password1': 'NHYU123',
-                'new_password2': 'NHYU123',
-            }
-        )
-
-        # 5) Now we continue with selenium, because the other
-        # means do not work
+        # 4) Now we make a new url password recovery with uid and token
         url_redefinition_password = reverse(password_reset_confirm_url_name,
                                             kwargs={
                                                 'uidb64': uid,
                                                 'token': token,
                                                 },
                                             )
-        browser = make_chrome_browser()
-        browser.get(self.live_server_url + url_redefinition_password)
-        input_new_password1 = browser.find_element(By.ID, 'id_new_password1')
-        input_new_password2 = browser.find_element(By.ID, 'id_new_password2')
-        form = browser.find_element(By.CLASS_NAME, 'C-form_register_form')
+
+        # Continue with Selenium
+        # 5) making a get request using the url above
+        self.browser.get(self.live_server_url + url_redefinition_password)
+
+        # 6) Getting the input password and form
+        input_new_password1 = self.browser.find_element(By.ID,
+                                                        'id_new_password1',
+                                                        )
+        input_new_password2 = self.browser.find_element(By.ID,
+                                                        'id_new_password2',
+                                                        )
+        form = self.browser.find_element(By.CLASS_NAME, 'C-form_register_form')
+
+        # 7) Setting the new password and making form submit
         new_password: str = 'Password123'
         input_new_password1.send_keys(new_password)
         input_new_password2.send_keys(new_password)
         form.submit()
 
-        # 6) Checking if the password has been reset
+        # 8) Checking if the password has been reset
         user = User.objects.get(id=self.user.id)
         password_changed: bool = user.check_password(new_password)
-        message_succes = browser.find_element(By.CLASS_NAME, 'message_success')
+        message_succes = self.browser.find_element(By.CLASS_NAME,
+                                                   'message_success',
+                                                   )
         self.assertTrue(password_changed)
         self.assertEqual(
             message_succes.text,
             'Sua senha foi alterada com sucesso.'
         )
-
-        # quit de broser
-        browser.quit()
-        self.fail(
-            'testar as mensagens de erro durante a recuperação de senha. '
-            'testar as mesanges usando métodos sem follow. '
-            'registrar que, usando follow temos acesso às messages, '
-            'e sem follow temos acesso aos kwargs (uid e token). '
-            'registrar todo o processo de teste de recuperação de senha. '
-            )
