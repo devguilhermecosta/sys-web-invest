cc696560263ca4d72b636672655054899cc0274b
Guilherme Costa <guilherme.partic@gmail.com>
2023-06-08 17:38:58 -0300

    test: created login tests

--- dashboard/tests/tests_login_view.py
+++ dashboard/tests/tests_login_view.py
@@ -1,22 +1,26 @@
 from django.test import TestCase
 from django.test import Client
 from django.urls import reverse
+from django.contrib.auth.models import User
 from parameterized import parameterized
 
 
 class LoginViewTests(TestCase):
     def setUp(self):
         self.user_data = {
-            'user': 'user',
-            'password': 'password',
+            'username': 'jhondoe',
+            'email': 'jhon@email.com',
+            'password': 'jhon123',
         }
+
+        self.c = Client()
         return super().setUp()
 
     @parameterized.expand([
         ('user', 'Campo obrigatório'),
         ('password', 'Campo obrigatório'),
     ])
-    def test_login_form_returns_message_error_if_fields_length_igual_zero(self, field, message) -> None:  # noqa: E501
+    def test_login_view_returns_message_error_if_fields_length_igual_zero(self, field, message) -> None:  # noqa: E501
         self.user_data[field] = ''
         c = Client()
 
@@ -27,7 +31,45 @@ class LoginViewTests(TestCase):
         )
 
         self.assertIn(message, response.content.decode('utf-8'))
-        self.fail(
-            'continuar a partir daqui. '
-            'Testar a url, view, status_code, etc.'
-            )
+        self.assertRedirects(response, '/', 302)
+
+    def test_login_view_returns_invalid_user_if_the_credentials_are_incorrects(self) -> None:  # noqa: E501
+        response = self.c.post(
+            reverse('dashboard:home'),
+            {
+                'user': self.user_data['username'],
+                'password': 'any',
+            },
+            follow=True,
+        )
+        self.assertIn(
+            'Usuário ou senha incorretos',
+            response.content.decode('utf-8'),
+        )
+        self.assertRedirects(response, '/', 302)
+
+    def test_login_view_returns_login_succesfully_if_credentials_are_correct_and_user_i_activate(self) -> None:  # noqa: E501
+        '''
+            This test create a new user and make login
+        '''
+
+        User.objects.create_user(
+            **self.user_data
+        )
+
+        response = self.c.post(
+            reverse('dashboard:home'),
+            {
+                'user': self.user_data['username'],
+                'password': self.user_data['password'],
+            },
+            follow=True,
+        )
+
+        self.assertIn('login realizado com sucesso',
+                      response.content.decode('utf-8'),
+                      )
+        self.assertRedirects(response,
+                             reverse('dashboard:login'),
+                             302,
+                             )
