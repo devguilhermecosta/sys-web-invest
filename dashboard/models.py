041acf3c5bf93267aedf9f5a7509acc4833cc62b
Guilherme Costa <guilherme.partic@gmail.com>
2023-06-10 16:33:58 -0300

    update: modified return from post method into CreateProfile view

--- dashboard/models.py
+++ dashboard/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    cpf = models.CharField(max_length=11, unique=True)
+    cpf = models.CharField(max_length=50)
     adress = models.CharField(max_length=255)
     number = models.CharField(max_length=15)
     city = models.CharField(max_length=255)
