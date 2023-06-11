d9203bac423d88af5d38537d0ce0cf4af58fef35
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-06-11 17:54:07 -0300

    test: created the new tests

--- dashboard/tests/tests_create_profile.py
+++ dashboard/tests/tests_create_profile.py
@@ -17,7 +17,7 @@ class CreateProfileTests(TestCase):
             'email': 'jhon@email.com',
         }
         self.user = User.objects.create_user(**self.user_data)
-        self.url = reverse('dashboard:create_profile')
+        self.url_create_profile = reverse('dashboard:create_profile')
         self.c = Client()
 
         self.profile_data = {
@@ -32,18 +32,20 @@ class CreateProfileTests(TestCase):
         return super().setUp()
 
     def test_create_profile_url_is_correct(self) -> None:
-        self.assertEqual(self.url, '/dashboard/criar_perfil/')
+        self.assertEqual(self.url_create_profile, '/dashboard/criar_perfil/')
 
     def test_create_profile_view_is_correct(self) -> None:
-        response = resolve(self.url)
+        response = resolve(self.url_create_profile)
         self.assertEqual(response.func.view_class, views.CreateProfile)
 
-    def make_login(self):
+    def make_login(self, **kwargs):
         response = self.c.post(
             reverse('dashboard:home'),
             {
-                'user': self.user_data.get('username'),
-                'password': self.user_data.get('password'),
+                'user': kwargs.get('user', self.user_data.get('username')),
+                'password': kwargs.get('password',
+                                       self.user_data.get('password'),
+                                       ),
             },
             follow=True,
         )
@@ -51,7 +53,7 @@ class CreateProfileTests(TestCase):
 
     def test_create_profile_redirect_to_login_page_if_user_is_not_logged_in(self) -> None:  # noqa: E501
         response = self.c.get(
-            reverse('dashboard:create_profile'),
+            self.url_create_profile,
             follow=True,
         )
 
@@ -65,14 +67,14 @@ class CreateProfileTests(TestCase):
         # first we make the login
         self.make_login()
 
-        response = self.c.get(self.url)
+        response = self.c.get(self.url_create_profile)
         self.assertTemplateUsed(response,
                                 'dashboard/pages/profile.html',
                                 )
 
     def test_create_profile_get_request_have_status_code_200(self) -> None:
         self.make_login()
-        response = self.c.get(self.url)
+        response = self.c.get(self.url_create_profile)
         self.assertEqual(response.status_code, 200)
 
     @parameterized.expand([
@@ -86,7 +88,7 @@ class CreateProfileTests(TestCase):
     def test_create_profile_get_request_load_correct_content(self, label) -> None:  # noqa: E501
         self.make_login()
 
-        response = self.c.get(self.url)
+        response = self.c.get(self.url_create_profile)
         content = response.content.decode('utf-8')
 
         self.assertIn(
@@ -117,7 +119,7 @@ class CreateProfileTests(TestCase):
         )
         self.assertRedirects(
             response,
-            reverse('dashboard:create_profile'),
+            self.url_create_profile,
             302,
         )
 
@@ -161,7 +163,7 @@ class CreateProfileTests(TestCase):
 
         # make get request in create profile page
         response = self.c.get(
-            reverse('dashboard:create_profile'),
+            self.url_create_profile,
             follow=True,
         )
 
@@ -193,6 +195,10 @@ class CreateProfileTests(TestCase):
             follow=True,
         )
         self.assertIn(message, response.content.decode('utf-8'))
+        self.assertRedirects(response,
+                             self.url_create_profile,
+                             302,
+                             )
 
     def test_create_profile_returns_error_message_if_cpf_is_invalid(self) -> None:  # noqa: E501
         # make login
@@ -203,14 +209,14 @@ class CreateProfileTests(TestCase):
 
         # try create profile
         response = self.c.post(
-            reverse('dashboard:create_profile'),
+            self.url_create_profile,
             data=self.profile_data,
             follow=True,
         )
 
         self.assertIn('CPF inválido', response.content.decode('utf-8'))
 
-    def test_create_profile_returns_error_message_if_cpf_length_less_then_11(self) -> None:  # noqa: E501
+    def test_create_profile_returns_error_message_if_cpf_length_less_then_21(self) -> None:  # noqa: E501
         # make login
         self.make_login()
 
@@ -219,14 +225,18 @@ class CreateProfileTests(TestCase):
 
         # try create profile
         response = self.c.post(
-            reverse('dashboard:create_profile'),
+            self.url_create_profile,
             data=self.profile_data,
             follow=True,
         )
 
         self.assertIn('CPF inválido', response.content.decode('utf-8'))
+        self.assertRedirects(response,
+                             self.url_create_profile,
+                             302,
+                             )
 
-    def test_create_profile_returns_a_new_profile_if_all_ok(self) -> None:  # noqa: E501
+    def test_create_profile_returns_a_new_profile_if_all_data_is_ok(self) -> None:  # noqa: E501
         # make login
         self.make_login()
 
@@ -235,7 +245,7 @@ class CreateProfileTests(TestCase):
 
         # try create profile
         response = self.c.post(
-            reverse('dashboard:create_profile'),
+            self.url_create_profile,
             data=self.profile_data,
             follow=True,
         )
@@ -244,11 +254,60 @@ class CreateProfileTests(TestCase):
             'Perfil criado com sucesso',
             response.content.decode('utf-8'),
             )
-        self.assertRedirects(
-            response,
-            reverse('dashboard:user_dashboard'),
-            302,
-        )
+        self.assertRedirects(response,
+                             reverse('dashboard:user_dashboard'),
+                             302,
+                             )
 
     def test_create_profile_returns_error_message_if_cpf_is_already_in_use(self) -> None:  # noqa: E501
-        self.fail('O código está funcionando, mas o teste não')
+        # create first user
+        user_1 = User.objects.create_user(username='jhondoe2',
+                                          email='jhon@email.com',
+                                          password='jhon123456',
+                                          )
+
+        # try make login
+        self.make_login(user='jhondoe2', password='jhon123456')
+
+        # profile data
+        profile_data = {
+            'user': user_1,
+            'cpf': '075.813.310-32',
+            'adress': 'street',
+            'number': '10',
+            'city': 'new york',
+            'uf': 'ny',
+            'cep': '85601231',
+            }
+
+        # create the profile for first user
+        self.c.post(
+            self.url_create_profile,
+            profile_data,
+            follow=True,
+        )
+
+        # create second user
+        user_2 = User.objects.create_user(username='jhondoe3',
+                                          email='jhon3@email.com',
+                                          password='jhon123456',
+                                          )
+
+        # make login with second user
+        self.make_login(user='jhondoe3', password='jhon123456')
+
+        # set profile_data
+        profile_data['user'] = user_2
+
+        # tyr create the profile for second user with same CPF
+        response = self.c.post(
+            self.url_create_profile,
+            profile_data,
+            follow=True,
+        )
+
+        self.assertIn('CPF já cadastrado', response.content.decode('utf-8'))
+        self.assertRedirects(response,
+                             self.url_create_profile,
+                             302,
+                             )
