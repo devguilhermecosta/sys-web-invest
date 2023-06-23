7c420841e0e2950d01161ae4e56910d73133f7ef
Guilherme Costa <guilherme.partic@gmail.com>
2023-06-23 18:17:19 -0300

    feat: add AJAX to CEP

--- base_static/global/js/script.js
+++ base_static/global/js/script.js
@@ -10,27 +10,56 @@
       labelSister.classList.add(className);
     }
 
-    input.addEventListener('change', function() {
-      const inputLengthInside = input.value.length;
-      if (inputLengthInside >= 1) {
-        labelSister.classList.add(className);
-      } else {
-        labelSister.classList.remove(className);
-      }
-    })
+    try {
+      input.addEventListener('change', function() {
+        const inputLengthInside = input.value.length;
+        if (inputLengthInside >= 1) {
+          labelSister.classList.add(className);
+        } else {
+          labelSister.classList.remove(className);
+        }
+      }) 
+    }
+    catch(e){}
   })
 })();
 
 // change eye from dashboard intro
 (() => {
-  const eyeInput = document.querySelector('#show-invest');
-  const eyeIcon = document.querySelector('.eye-icon');
-
-  eyeInput.addEventListener('change', function() {
-    if (eyeInput.checked) {
-      eyeIcon.innerHTML = 'visibility_off';
-    } else if (!eyeInput.checked) {
-      eyeIcon.innerHTML = 'visibility';
-    }
-  })
+  try{
+    const eyeInput = document.querySelector('#show-invest');
+    const eyeIcon = document.querySelector('.eye-icon');
+  
+    eyeInput.addEventListener('change', function() {
+      if (eyeInput.checked) {
+        eyeIcon.innerHTML = 'visibility_off';
+      } else if (!eyeInput.checked) {
+        eyeIcon.innerHTML = 'visibility';
+      }
+    })
+  }catch(e){};
 })();
+
+//
+
+const inputCep = document.getElementById('id_cep');
+inputCep.addEventListener('change', searchCep);
+
+function searchCep() {
+  let cep = inputCep.value.replace('-', '');
+  let url = 'http://viacep.com.br/ws/' + cep + '/json';
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        json = JSON.parse(xhr.responseText)
+        console.log(json);
+      }
+    }
+  }
+  xhr.send();
+}
+
+// https://blog.matheuscastiglioni.com.br/requisicoes-ajax-com-javascript/#:~:text=O%20Javascript%20nativo%20possui%20um,protoc%C3%B3los%20HTTP%2C%20FILE%20e%20FTP.
