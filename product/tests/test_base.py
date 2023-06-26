524d7facc0247836f4f818b85c831f9779cac950
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-06-26 16:58:09 -0300

    test: created the function for create a simple file test

---
+++ product/tests/test_base.py
@@ -0,0 +1,15 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
+from pathlib import Path
+
+
+def make_simple_file() -> SimpleUploadedFile:
+    path = Path(__file__).parent.parent.parent
+    file_path = ''.join(
+        (str(path), '/product/tests/file_test.pdf')
+    )
+    simple_file = SimpleUploadedFile(
+        name='file_test.pdf',
+        content=open(file_path, 'rb').read(),
+        content_type='file/pdf'
+    )
+    return simple_file
