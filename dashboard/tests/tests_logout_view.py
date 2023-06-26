109220a621529f63dafe36b793f2b0085194340a
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-06-26 17:21:32 -0300

    test: created the user logout tests

--- dashboard/tests/tests_logout_view.py
+++ dashboard/tests/tests_logout_view.py
@@ -1,6 +1,39 @@
-from django.test import TestCase
+from django.urls import resolve, reverse
+
+from dashboard.views import LogoutView
+from utils.mixins.auth import TestCaseWithLogin
 
 
-class LogoutViewTests(TestCase):
-    def test_the_test(self):
-        self.fail('testar o logout')
+class LogoutViewTests(TestCaseWithLogin):
+    url = reverse('dashboard:logout')
+
+    def test_logout_url_is_correct(self) -> None:
+        self.assertEqual(
+            self.url,
+            '/logout/'
+        )
+
+    def test_logout_loads_correct_view(self) -> None:
+        response = resolve(self.url)
+        self.assertIs(response.func.view_class, LogoutView)
+
+    def test_logout_returns_status_code_405_if_get_request(self) -> None:
+        ''' 405 - method not allowed '''
+        response = self.client.get(self.url, follow=True)
+        self.assertEqual(response.status_code, 405)
+
+    def test_logout_returns_message_warning_if_logout_succesfully(self) -> None:  # noqa: E501
+        # make login
+        self.make_login()
+
+        response = self.client.post(self.url, follow=True)
+
+        self.assertRedirects(
+            response,
+            '/',
+            302,
+        )
+        self.assertIn(
+            'Logout realizado com sucesso',
+            response.content.decode('utf-8')
+        )
