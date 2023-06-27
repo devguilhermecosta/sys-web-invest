1a59554f55f7886b1786fc70c014e0f5915a521c
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-06-26 23:47:48 -0300

    conf: config media url and media root

--- syswebinvest/settings.py
+++ syswebinvest/settings.py
@@ -16,7 +16,6 @@ from django.contrib.messages import constants as message
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -123,12 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     BASE_DIR / "base_static",
 ]
-STATIC_ROOT = "static/"
-MEDIA_ROOT = "media/"
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
