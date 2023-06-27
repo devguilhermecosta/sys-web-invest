7971748618036af2eb6456bcf321c01561dea7a2
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-06-26 23:47:19 -0300

    conf: add static url and static root for files

--- syswebinvest/urls.py
+++ syswebinvest/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('__debug__/', include('debug_toolbar.urls')),
@@ -25,3 +27,9 @@ urlpatterns = [
     path('ativos/', include('product.urls')),
     path('', include('dashboard.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+        )
