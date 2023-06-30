1bb61e527513b9f51a5047869facecd36a739cd7
Guilherme Costa <guilherme.partic@gmail.com>
2023-06-29 23:56:04 -0300

    update: add a .parent into make_simple_file

--- product/tests/actions/test_base.py
+++ product/tests/actions/test_base.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 
 def make_simple_file() -> SimpleUploadedFile:
-    path = Path(__file__).parent.parent.parent
+    path = Path(__file__).parent.parent.parent.parent
     file_path = ''.join(
-        (str(path), '/product/tests/file_test.pdf')
+        (str(path), '/product/tests/base_tests/file_test.pdf')
     )
     simple_file = SimpleUploadedFile(
         name='file_test.pdf',
