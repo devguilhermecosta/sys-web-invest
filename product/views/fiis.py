100098e1e56e039905c0771f18ce6b660c0e0a08
Guilherme Costa <guilherme.partic@gmail.com>
2023-07-01 13:57:48 -0300

    update: updated the FiiBuyView

--- product/views/fiis.py
+++ product/views/fiis.py
@@ -1,13 +1,13 @@
 from django.views import View
 from django.views.generic import ListView
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from product.forms import FIIBuyForm
-from product.models import FII, UserFII
+from product.models import FII, UserFII, FiiHistory
 
 
 @method_decorator(
@@ -47,6 +47,21 @@ class AllFIIsView(ListView):
 
 
 class FIISBuyView(FIIsView):
+    def success_response(self, qty: int, code: str) -> HttpResponseRedirect:
+        messages.success(
+            self.request,
+            (
+                f'compra de {qty} unidade(s) de {code.upper()} '
+                'realizada com sucesso'
+            )
+        )
+
+        del self.request.session['fiis-buy']
+
+        return redirect(
+            reverse('product:fiis')
+        )
+
     def get(self, *args, **kwargs) -> HttpResponse:
         session = self.request.session.get('fiis-buy', None)
         form = FIIBuyForm(session)
@@ -63,47 +78,53 @@ class FIISBuyView(FIIsView):
     def post(self, *args, **kwargs) -> HttpResponse:
         post = self.request.POST
         self.request.session['fiis-buy'] = post
-        form = FIIBuyForm(post)
+        form = FIIBuyForm(
+            data=post or None,
+            files=self.request.FILES or None,
+        )
 
         if form.is_valid():
             data = form.cleaned_data
-            code = data['code']
-            qty = int(data['quantity'])
-            up = float(data['unit_price'])
+            params = {
+                'quantity': int(data['quantity']),
+                'unit_price': float(data['unit_price']),
+                'date': data['date'],
+            }
+            trading_note = data.get('trading_note', None)
             user = self.request.user
-            fii = FII.objects.filter(code=code).first()
+            fii = FII.objects.filter(code=data['code']).first()
 
-            user_fii = UserFII.objects.filter(
+            user_fii_exisits = UserFII.objects.filter(
                 fii=fii,
                 user=user,
-            )
+            ).first()
 
-            if user_fii.exists():
-                actual_user_fii = user_fii.first()
-                actual_user_fii.quantity += qty
-                actual_user_fii.unit_price = up
-                actual_user_fii.save()
-
-            new_fii = UserFII.objects.create(
-                user=user,
-                fii=fii,
-                quantity=qty,
-                unit_price=up,
-            )
-            new_fii.save()
-
-            messages.success(
-                self.request,
-                (
-                    f'compra de {qty} unidade(s) de {code.upper()} '
-                    'realizada com sucesso'
+            if user_fii_exisits:
+                user_fii_exisits.buy(
+                    trading_note=trading_note, **params,
                 )
+                return self.success_response(
+                    qty=params['quantity'], code=fii.code,
+                )
+
+            new_user_fii = UserFII.objects.create(
+                user=user,
+                fii=fii,
+                **params,
             )
+            new_user_fii.save()
 
-            del self.request.session['fiis-buy']
+            fii_history = FiiHistory.objects.create(
+                userfii=new_user_fii,
+                handler='buy',
+                total_price=params['quantity'] * params['unit_price'],
+                trading_note=trading_note,
+                **params,
+            )
+            fii_history.save()
 
-            return redirect(
-                reverse('product:fiis')
+            return self.success_response(
+                qty=params['quantity'], code=fii.code,
             )
 
         return redirect(
