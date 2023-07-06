bb4372daca77d635afca2bf680ae6ad816c2cb9b
Guilherme Costa <guilherme.partic@gmail.com>
2023-07-06 19:41:19 -0300

    test: created the new test

--- product/tests/fixed_income/tests_fixed_income.py
+++ product/tests/fixed_income/tests_fixed_income.py
@@ -87,4 +87,71 @@ class FixedIncomeTests(TestCaseWithLogin):
         self.assertEqual(len(queryset), 1)
 
     def test_fixed_income_loads_just_products_of_authenticated_user(self) -> None:  # noqa: E501
-        self.fail('continuar daqui')
+        # create a new user
+        user_one = self.create_user(True,
+                                    username='userone',
+                                    password='userone@email.com',
+                                    )
+
+        # make fixed income with user_one
+        make_fixed_income_product(user=user_one,
+                                  category='lci',
+                                  name='lci fazendinha test',
+                                  )
+
+        # make login with username user
+        _, user = self.make_login()
+
+        # make fixed income with user
+        make_fixed_income_product(user=user,
+                                  category='cdb',
+                                  name='cdb c6 banck',
+                                  )
+
+        # get all fixed income objects
+        fixed_income_objs = ProductFixedIncome.objects.all()
+
+        # checks if the two objects has been created
+        self.assertEqual(len(fixed_income_objs), 2)
+        self.assertEqual(fixed_income_objs[0].name, 'lci fazendinha test')
+        self.assertEqual(fixed_income_objs[0].category, 'lci')
+        self.assertEqual(fixed_income_objs[1].name, 'cdb c6 banck')
+        self.assertEqual(fixed_income_objs[1].category, 'cdb')
+
+        # access fixed income page with user
+        response = self.client.get(self.url)
+        content = response.content.decode('utf-8')
+
+        # checks if the user has the correct object
+        self.assertIn(
+            'cdb c6 banck',
+            content,
+        )
+        self.assertNotIn(
+            'lci fazendinha test',
+            content,
+        )
+
+        # NOW I'M GOING TO TEST IF THE USER'S PRODUCT IS IN USER_ONE
+
+        # make logout with user
+        self.client.post(
+            reverse('dashboard:logout')
+        )
+
+        # make login with user_one
+        self.make_login(create_user=False, username='userone')
+
+        # access fixed income page
+        response_2 = self.client.get(self.url)
+        content_2 = response_2.content.decode('utf-8')
+
+        # checks if the user_one has the correct object
+        self.assertIn(
+            'lci fazendinha test',
+            content_2,
+        )
+        self.assertNotIn(
+            'cdb c6 banck',
+            content_2
+        )
