5b81909b80b794efa6602f1f4b4144a9637e5705
Guilherme Costa <guilherme.partic@gmail.com>
2023-07-06 22:07:02 -0300

    feat: created the product fixed income form

--- product/forms/fixed_income/fixed_income.py
+++ product/forms/fixed_income/fixed_income.py
@@ -1,8 +1,57 @@
+from typing import Any, Dict
 from django import forms
+from django.core.exceptions import ValidationError
 from product.models import ProductFixedIncome
 
 
-class FixedIncomeForm(forms.ModelForm):
+class FixedIncomeRegisterForm(forms.ModelForm):
+    category = forms.CharField(
+        required=False,
+        label='categoria',
+    )
+    name = forms.CharField(
+        required=False,
+        label='nome',
+    )
+    value = forms.FloatField(
+        required=False,
+        label='valor',
+    )
+    grace_period = forms.CharField(
+        required=False,
+        label='carência',
+        widget=forms.DateInput(
+            attrs={
+                'type': 'date',
+            }
+        )
+    )
+    maturity_date = forms.DateField(
+        required=False,
+        label='vencimento',
+        widget=forms.DateInput(
+            attrs={
+                'type': 'date',
+            }
+        )
+    )
+    liquidity = forms.CharField(
+        required=False,
+        label='liquidez',
+    )
+    profitability = forms.CharField(
+        required=False,
+        label='rentabilidade',
+    )
+    interest_receipt = forms.CharField(
+        required=False,
+        label='pagamento de juros',
+    )
+    description = forms.CharField(
+        required=False,
+        label='observações',
+    )
+
     class Meta:
         model = ProductFixedIncome
         fields = [
@@ -16,3 +65,17 @@ class FixedIncomeForm(forms.ModelForm):
             'interest_receipt',
             'description',
         ]
+
+    def clean(self) -> Dict[str, Any]:
+        data = self.cleaned_data
+
+        for key, value in data.items():
+            if not value or value == '':
+                raise ValidationError(
+                    {
+                        key: 'Campo obrigatório',
+                    },
+                    code='required',
+                )
+
+        return super().clean()
