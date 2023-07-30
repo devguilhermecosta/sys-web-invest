5e996c0ba160f8a1c9bde4f468323e6a2a5c2888
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-07-30 14:05:28 -0300

    update: changed the name of form

--- product/forms/__init__.py
+++ product/forms/__init__.py
@@ -1,5 +1,6 @@
 # flake8: noqa
 
 from .actions.action_form import ActionBuyAndSellForm
+from .actions.profits import ActionsReceivProfitsForm
 from .fiis.fii_buy import FIIBuyForm
 from .fiis.profits import FIIReceiptProfitsForm
