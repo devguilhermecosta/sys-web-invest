cac1788e5374bad0eb076ddbaba8e47e8d32910c
Guilherme Costa <guilherme.partic@gmail.com>
2023-07-30 00:00:39 -0300

    test: created the new test

--- product/tests/fiis/tests_fiis_profits_history.py
+++ product/tests/fiis/tests_fiis_profits_history.py
@@ -82,3 +82,102 @@ class FIISProfitsHistoryTests(TestCaseWithLogin):
              ),
             response.content.decode('utf-8')
         )
+
+    def test_fiis_profits_history_returns_a_json_response_with_correct_data_only_from_the_logged_in_user(self) -> None:  # noqa: E501
+        # create another user
+        another_user = self.create_user(
+            username='jhondoe',
+            email='jhondoe@email.com',
+        )
+
+        # create the UserFII for another_user
+        another_userfii = make_user_fii(
+            another_user,
+            1,
+            1,
+            'pvbi11',
+            'teste',
+        )
+
+        # make login with the another_user
+        self.make_login(create_user=False, username=another_user)
+
+        # make post request for create the history for another_userfii
+        r_post = self.client.post(
+            reverse('product:fiis_manage_income_receipt'),
+            {
+                'user_product_id': another_userfii.id,
+                'value': 50,
+                'date': '2023-07-02',
+            },
+            follow=True,
+        )
+
+        # checks if the history has been created
+        self.assertEqual(
+            r_post.content.decode('utf-8'),
+            '{"success": "success request"}',
+        )
+
+        # make get request with another_user
+        response_another_user = self.client.get(self.url)
+
+        expected_another_user_content = (
+            '{"date": "2023-07-02", "history_id": 1, '
+            '"product": "pvbi11", "value": 50.0, '
+            '"handler": "profits"}'
+        )
+
+        # checks if the response for another_user is correct
+        self.assertIn(
+            expected_another_user_content,
+            response_another_user.content.decode('utf-8')
+        )
+
+        # make logout with the another_user
+        self.client.logout()
+
+        #########
+
+        # make login with user=user
+        _, user = self.make_login()
+
+        # make the user fii
+        user_fii = make_user_fii(
+            user=user,
+            qty=1,
+            unit_price=10,
+            code='mxrf11',
+            desc='maxi renda',
+        )
+
+        # data
+        data = {
+            'user_product_id': user_fii.pk,
+            'date': '2023-07-02',
+            'value': 4.96,
+        }
+
+        # make post request for save the profits
+        self.client.post(
+            reverse('product:fiis_manage_income'),
+            data=data,
+        )
+
+        # make get request with user=user
+        response = self.client.get(self.url)
+
+        # checks if the content is correct
+        self.assertIn(
+            ('{"data": [{"date": "2023-07-02", '
+             '"history_id": 2, "product": "mxrf11", '
+             '"value": 4.96, "handler": "profits"}]}'
+             ),
+            response.content.decode('utf-8')
+        )
+
+        # checks if the content of another_user is not in the user's response
+        self.assertNotIn(
+            expected_another_user_content,
+            response.content.decode('utf-8'),
+        )
