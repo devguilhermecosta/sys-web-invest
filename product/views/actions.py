5e20e761de67d1656825d8d36badbfa3c1996eff
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-07-30 14:07:29 -0300

    update: created the post method for ActionsProfitsViews

--- product/views/actions.py
+++ product/views/actions.py
@@ -1,10 +1,12 @@
 from django.views import View
 from django.views.generic import ListView
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from product.forms import ActionBuyAndSellForm
+from django.contrib import messages
+from product.forms import ActionBuyAndSellForm, ActionsReceivProfitsForm
 from product.models import Action, UserAction, ActionHistory
 from .base_views.variable_income import Buy, Sell, History
 
@@ -74,6 +76,69 @@ class ActionHistoryDetails(History):
     history_model = ActionHistory
 
 
-class ActionsManageIncomeView(ActionsView):
+class ActionsManageProfitsView(ActionsView):
+    def choices(self) -> tuple:
+        choices = [('---', '---')]
+
+        objects = UserAction.objects.filter(
+            user=self.request.user
+        )
+
+        for user_action in objects:
+            choices.append(
+                (user_action.id, user_action.product.code)
+            )
+
+        return choices
+
     def get(self, *args, **kwargs) -> HttpResponse:
-        ...
+        session = self.request.session.get('action-receiv-profits', None)
+        form = ActionsReceivProfitsForm(session)
+        form.fields.get('user_product_id').widget.choices = self.choices()
+
+        return render(
+            self.request,
+            'product/pages/actions/actions_profits.html',
+            context={
+                'form': form,
+                'form_title': 'lançar rendimento',
+                'button_submit_value': 'salvar',
+            }
+        )
+
+    def post(self, *args, **kwargs) -> HttpResponse:
+        post = self.request.POST
+        self.request.session['action-receiv-profits'] = post
+        form = ActionsReceivProfitsForm(post)
+
+        if form.is_valid():
+            data = form.cleaned_data
+
+            user_action = get_object_or_404(
+                UserAction,
+                pk=data['user_product_id'],
+            )
+
+            if user_action.user != self.request.user:
+                raise Http404()
+
+            user_action.receiv_profits(
+                handler=data['profits_type'],
+                date=data['date'],
+                total_price=data['total_price'],
+                tax_and_irpf=data.get('tax_and_irpf', ''),
+            )
+
+            del self.request.session['action-receiv-profits']
+
+            messages.success(
+                self.request,
+                (
+                    f'rendimento para {user_action.product.code} '
+                    f'lançado com sucesso',
+                )
+            )
+
+        return redirect(
+            reverse('product:actions_manage_profits')
+        )
