bc65b96c7a595fdf94d792e8f011c02893e4e880
Guilherme Costa <guilherme.partic@gmail.com>
2023-08-03 18:53:01 -0300

    update: add the locale module

--- product/templatetags/money.py
+++ product/templatetags/money.py
@@ -1,9 +1,11 @@
 from django import template
+import locale
 
+locale.setlocale(locale.LC_MONETARY, 'pt_BR.UTF8')
 
 register = template.Library()
 
 
 @register.filter('real_brl')
 def real_brl(value):
-    return (f'R$ {value:.2f}').replace('.', ',')
+    return locale.currency(value)
