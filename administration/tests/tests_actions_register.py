297e2d45e7efd09ad7b86a9bbf7c2f93b359440e
Guilherme Costa <guilherme.partic@gmail.com>
2023-08-12 23:18:45 -0300

    test: created the new tests

--- administration/tests/tests_actions_register.py
+++ administration/tests/tests_actions_register.py
@@ -1,5 +1,8 @@
+from django.urls import resolve, reverse
+from parameterized import parameterized
+
 from utils.mixins.auth import TestCaseWithLogin
-from django.urls import reverse, resolve
+
 from .. import views
 
 
@@ -19,5 +22,68 @@ class ActionsRegisterTests(TestCaseWithLogin):
             views.ActionRegister,
         )
 
+    def test_action_register_returns_status_code_302_if_the_user_is_not_authenticated(self) -> None:  # noqa: E501
+        # make get request without begins logged in
+        response = self.client.get(self.url)
+        self.assertRedirects(
+            response,
+            '/?next=/painel-de-controle/cadastrar/acao/',
+            302,
+        )
 
-# organizar as views das ações e fiis
+    def test_action_register_redirects_the_page_to_dashboard_if_the_user_is_not_staff(self) -> None:  # noqa: E501
+        # make login with a common user
+        self.make_login()
+
+        # make get request without begins logged in
+        response = self.client.get(self.url)
+        self.assertRedirects(
+            response,
+            '/dashboard/painel-do-usuario/',
+            302,
+        )
+
+    def test_action_register_returns_status_code_200_if_the_user_is_staff(self) -> None:  # noqa: E501
+        # make login with a user staff
+        self.make_login(is_staff=True)
+
+        # make get request
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_action_register_loads_correct_template(self) -> None:
+        # make login with a user staff
+        self.make_login(is_staff=True)
+
+        # make get request
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(
+            response,
+            'administration/pages/product_register.html',
+        )
+
+    @parameterized.expand([
+        'Registrar nova Ação',
+        'código',
+        'descrição',
+        'cnpj',
+        'registrar',
+    ])
+    def test_action_register_loads_correct_content(self, text: str) -> None:
+        # make login with a user staff
+        self.make_login(is_staff=True)
+
+        # make get request
+        response = self.client.get(self.url)
+        content = response.content.decode('utf-8')
+
+        self.assertIn(
+            text,
+            content,
+        )
+
+    def test_action_register_loads_the_registered_actions(self, text: str) -> None:  # noqa: E501
+        # make login with a user staff
+        self.make_login(is_staff=True)
+
+        self.fail('continuar daqui')
