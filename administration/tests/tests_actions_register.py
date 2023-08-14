8657a83edcb04f2de9804f0796695fccf90680a1
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-08-13 22:17:30 -0300

    test: created the new tests

--- administration/tests/tests_actions_register.py
+++ administration/tests/tests_actions_register.py
@@ -1,9 +1,9 @@
 from django.urls import resolve, reverse
 from parameterized import parameterized
-
 from utils.mixins.auth import TestCaseWithLogin
-
 from .. import views
+from product.tests.base_tests import make_action_in_batch, make_action
+from product.models import Action
 
 
 class ActionsRegisterTests(TestCaseWithLogin):
@@ -68,10 +68,15 @@ class ActionsRegisterTests(TestCaseWithLogin):
         'descrição',
         'cnpj',
         'registrar',
+        'editar',
+        'deletar',
     ])
     def test_action_register_loads_correct_content(self, text: str) -> None:
         # make login with a user staff
         self.make_login(is_staff=True)
+
+        # create an action
+        make_action('bbas3', 'banco do brasil')
 
         # make get request
         response = self.client.get(self.url)
@@ -82,8 +87,182 @@ class ActionsRegisterTests(TestCaseWithLogin):
             content,
         )
 
+    @parameterized.expand([
+        'bbas3',
+        'banco do brasil',
+        '00.000.000/0001-91',
+    ])
     def test_action_register_loads_the_registered_actions(self, text: str) -> None:  # noqa: E501
         # make login with a user staff
         self.make_login(is_staff=True)
 
-        self.fail('continuar daqui')
+        # create the 5 actions in batch
+        make_action_in_batch(5)
+
+        # create a individual action
+        make_action('bbas3', 'banco do brasil', cnpj='00.000.000/0001-91')
+
+        # make get request
+        response = self.client.get(self.url)
+        context = response.context
+        content = response.content.decode('utf-8')
+
+        # checks the actions context
+        self.assertEqual(
+            len(context['products']),
+            6,
+        )
+        self.assertIn(
+            text,
+            content,
+        )
+
+    @parameterized.expand([
+        ('code', 'Campo obrigatório'),
+        ('description', 'Campo obrigatório'),
+        ('cnpj', 'Campo obrigatório'),
+    ])
+    def test_action_register_returns_error_messages_if_any_field_is_empty(self, field: str, message: str) -> None:  # noqa: E501
+        # make login with user staff
+        self.make_login(is_staff=True)
+
+        # action data
+        data = {
+            field: '',
+        }
+
+        # make post request
+        response = self.client.post(
+            self.url,
+            data,
+            follow=True,
+        )
+        content = response.content.decode('utf-8')
+
+        self.assertIn(
+            message,
+            content,
+        )
+        self.assertRedirects(
+            response,
+            self.url,
+            302,
+        )
+
+    @parameterized.expand([
+        ('code', '123', 'O código deve ter 5 caracteres'),
+        ('description', '', 'A descrição deve ter pelo menos 3 caracteres'),
+        ('cnpj', '00.000.000/0001', 'Cnpj inválido'),
+    ])
+    def test_action_register_returns_error_messages_if_any_field_is_invalid(self, field: str, value: str, message: str) -> None:  # noqa: E501
+        # make login with user staff
+        self.make_login(is_staff=True)
+
+        # action data
+        data = {
+            field: value,
+        }
+
+        # make post request
+        response = self.client.post(
+            self.url,
+            data,
+            follow=True,
+        )
+        content = response.content.decode('utf-8')
+
+        self.assertIn(
+            message,
+            content,
+        )
+        self.assertRedirects(
+            response,
+            self.url,
+            302,
+        )
+
+    @parameterized.expand([
+        ('code', 'bbas3', 'Ação já registrada'),
+        ('description', 'banco do brasil', ''),
+        ('cnpj', '00.000.000/0001-91', 'CNPJ já registrado'),
+    ])
+    def test_action_register_returns_error_messages_if_any_data_is_already_in_use(self, field: str, value: str, message: str) -> None:  # noqa: E501
+        # make login with user staff
+        self.make_login(is_staff=True)
+
+        # create an action
+        make_action('bbas3', 'banco do brasil', '00.000.000/0001-91')
+
+        # action data
+        data = {
+            field: value,
+        }
+
+        # make post request
+        response = self.client.post(
+            self.url,
+            data,
+            follow=True,
+        )
+        content = response.content.decode('utf-8')
+
+        self.assertIn(
+            message,
+            content,
+        )
+        self.assertRedirects(
+            response,
+            self.url,
+            302,
+        )
+
+    def test_action_register_returns_success_message_if_all_fields_is_ok(self) -> None:  # noqa: E501
+        # make login with user staff
+        self.make_login(is_staff=True)
+
+        # action data
+        data = {
+            'code': 'bbas3',
+            'description': 'banco do brasil',
+            'cnpj': '00.000.000/0001-91',
+        }
+
+        # make post request
+        response = self.client.post(
+            self.url,
+            data,
+            follow=True,
+        )
+        content = response.content.decode('utf-8')
+
+        self.assertIn(
+            'Ação criada com sucesso',
+            content,
+        )
+        self.assertRedirects(
+            response,
+            self.url,
+            302,
+        )
+
+    def test_action_register_creates_a_new_action_object_if_all_field_is_ok(self) -> None:  # noqa: E501
+        # make login with user staff
+        self.make_login(is_staff=True)
+
+        # action data
+        data = {
+            'code': 'bbas3',
+            'description': 'banco do brasil',
+            'cnpj': '00.000.000/0001-91',
+        }
+
+        # make post request
+        self.client.post(
+            self.url,
+            data,
+            follow=True,
+        )
+
+        actions = Action.objects.all()
+
+        self.assertEqual(len(actions), 1)
