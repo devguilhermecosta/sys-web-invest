7bb4f480b4fa76f9c05b3daf853dfbff11e05e0e
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-08-16 23:13:33 -0300

    test

--- syswebinvest/settings/send_email.py
+++ syswebinvest/settings/send_email.py
@@ -10,4 +10,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'guilherme.partic@gmail.com'
-EMAIL_HOST_PASSWORD = 'gwitergdkdwehydv'
+EMAIL_HOST_PASSWORD = 'slatnkhovazmjkev'
