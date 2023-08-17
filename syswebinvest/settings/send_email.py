54ec9a79c67c48371155cb4d886ab898089ff8f2
Guilherme Costa <guilherme.partic@gmail.com>
2023-08-17 16:50:08 -0300

    update the HOST_PORT

--- syswebinvest/settings/send_email.py
+++ syswebinvest/settings/send_email.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND')
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_PORT = 587
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT'))
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
