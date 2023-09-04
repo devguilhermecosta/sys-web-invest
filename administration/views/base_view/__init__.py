fc553952df488f1a7d450ba2d750c3642a30c3ce
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-09-04 19:20:37 -0300

    feat: created the AutoRegisterView

--- administration/views/base_view/__init__.py
+++ administration/views/base_view/__init__.py
@@ -1,6 +1,7 @@
 # flake8: noqa
 
 from .register import Register
+from .auto_register import AutoRegister
 from .update import Update
 from .delete import Delete
 from .update_last_close_value import UpdateLastClose
