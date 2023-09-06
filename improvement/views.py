5734035cc6c475e8d1ef0d76dbddb6dbd007edc8
Guilherme Costa <guilherme.partic@gmail.com>
2023-09-06 18:04:22 -0300

    update

--- improvement/views.py
+++ improvement/views.py
@@ -1,6 +1,11 @@
 from django.views import View
 from django.http import Http404, HttpResponse
+from django.shortcuts import redirect
+from django.urls import reverse
 from django.core.mail import send_mail
+from django.conf import settings
+from django.template.loader import render_to_string
+from django.contrib import messages
 
 
 class SendEmailView(View):
@@ -8,6 +13,33 @@ class SendEmailView(View):
         raise Http404()
 
     def post(self, *args, **kwargs) -> HttpResponse:
-        post = self.request.POST
-        print(post)
-        return HttpResponse('este é o content')
+        user = self.request.user
+        data = self.request.POST
+        subject = data.get('subject')
+        user_message = data.get('content')
+        message = render_to_string(
+            'improvement/pages/mail_message.html',
+            {
+                'user_full_name': user.first_name + ' ' + user.last_name,
+                'username': user.username,
+                'user_email': user.email,
+                'ticket_name': subject,
+                'ticket_description': user_message,
+            }
+        )
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[settings.EMAIL_HOST_USER],
+            fail_silently=True,
+        )
+
+        messages.success(
+            self.request,
+            'Solicitação enviada com sucesso.'
+        )
+
+        return redirect(
+            reverse('dashboard:user_dashboard')
+        )
