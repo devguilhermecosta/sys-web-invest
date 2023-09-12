520a6dae4fd2c430f2adcba67c5d2b56b8b1fe37
Guilherme Costa <guilherme.partic@gmail.com>
2023-09-12 14:27:32 -0300

    feat: created the Improvement model

--- improvement/models.py
+++ improvement/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Improvement(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=560)
+    status = models.CharField(max_length=50, choices=(
+        ('enviado', 'enviado'),
+        ('em análise', 'em análise'),
+        ('em desenvolvimento', 'em desenvolvimento'),
+        ('recusado', 'recusado'),
+        ('concluído', 'concluído'),
+    ))
+
+    def __str__(self) -> str:
+        return self.title
--- improvement/views.py
+++ improvement/views.py
@@ -6,8 +6,15 @@ from django.core.mail import send_mail
 from django.conf import settings
 from django.template.loader import render_to_string
 from django.contrib import messages
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from .models import Improvement
 
 
+@method_decorator(
+    login_required(redirect_field_name='next', login_url='/'),
+    name='dispatch',
+)
 class SendEmailView(View):
     def get(self, *args, **kwargs) -> None:
         raise Http404()
@@ -17,29 +24,39 @@ class SendEmailView(View):
         data = self.request.POST
         subject = data.get('subject')
         user_message = data.get('content')
-        message = render_to_string(
-            'improvement/pages/mail_message.html',
-            {
-                'user_full_name': user.first_name + ' ' + user.last_name,
-                'username': user.username,
-                'user_email': user.email,
-                'ticket_name': subject,
-                'ticket_description': user_message,
-            }
-        )
-        send_mail(
-            subject=subject,
-            message=message,
-            from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[settings.EMAIL_HOST_USER],
-            fail_silently=True,
-        )
 
-        messages.success(
-            self.request,
-            'Solicitação enviada com sucesso.'
-        )
+        if subject and user_message:
+            message = render_to_string(
+                'improvement/pages/mail_message.html',
+                {
+                    'user_full_name': user.first_name + ' ' + user.last_name,
+                    'username': user.username,
+                    'user_email': user.email,
+                    'ticket_name': subject,
+                    'ticket_description': user_message,
+                }
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=[settings.EMAIL_HOST_USER],
+                fail_silently=True,
+            )
 
-        return redirect(
-            reverse('dashboard:user_dashboard')
-        )
+            new_task = Improvement.objects.create(
+                user=user,
+                title=subject,
+                description=user_message,
+                status='enviado',
+            )
+            new_task.save()
+
+            messages.success(
+                self.request,
+                'Solicitação enviada com sucesso.'
+            )
+
+            return redirect(
+                reverse('dashboard:user_dashboard')
+            )
